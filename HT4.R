7c2191dd98218581972ad268ea522aab5afb7495
Pablo Josue Noack Cajbon <noa17596@uvg.edu.gt>
2021-03-18 15:49:44 -0600

    Hola

---
+++ HT4.R
@@ -0,0 +1,22 @@
+
+
+setwd("C:/Users/LENOVO/Desktop/Clases/Minería de datos/Github/HT3.Arboles_de_decision")
+datatest <- read.csv("house-prices-advanced-regression-techniques/test.csv")
+datatrain <- read.csv("house-prices-advanced-regression-techniques/train.csv")
+
+prices <- read.csv("house-prices-advanced-regression-techniques/sample_submission.csv")
+
+numstest <- sapply(datatest, is.numeric)
+numtrain <-sapply(datatrain, is.numeric)
+
+cdatatest <- datatest[,numstest]
+cdatatrain <- datatrain[,numtrain]
+names(cdatatest)
+names(cdatatrain)
+cdatatrain$SalePrice <- NULL
+
+cdata <- bind_rows(cdatatest,cdatatrain)
+cuantitativas_precios<-merge(x = cdata, y = prices, by = "Id")
+cuantitativas_precios <-  na.omit(cuantitativas_precios)
+
+write.csv(cuantitativas_precios,"C:/Users/LENOVO/Desktop/Clases/Minería de datos/cuantitativas_precios.csv")
