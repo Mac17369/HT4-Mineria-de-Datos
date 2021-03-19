2d61fde0239eb2450b32035f03758638bdf48893
Mac17369 <Mac17369@uvg.edu.gt>
2021-03-18 20:04:05 -0600

    caca

--- HT4.R
+++ HT4.R
@@ -1,9 +1,11 @@
-##setwd("C:/Users/Kevin Macario/Desktop/Uvg/9no Semestre/Mineria de Datos/HT4-Mineria-de-Datos")
-setwd("C:/Users/LENOVO/Desktop/Clases/Minería de datos/Github/HT4-Mineria-de-Datos")
+setwd("C:/Users/Kevin Macario/Desktop/Uvg/9no Semestre/Mineria de Datos/HT4-Mineria-de-Datos")
+#setwd("C:/Users/LENOVO/Desktop/Clases/Minería de datos/Github/HT4-Mineria-de-Datos")
 datatest <- read.csv("house-prices-advanced-regression-techniques/test.csv")
 datatrain <- read.csv("house-prices-advanced-regression-techniques/train.csv")
 library(dplyr)
 prices <- read.csv("house-prices-advanced-regression-techniques/sample_submission.csv")
+
+set.seed(666)
 
 numstest <- sapply(datatest, is.numeric)
 numtrain <-sapply(datatrain, is.numeric)
@@ -21,15 +23,36 @@ cuantitativas_precios <-  na.omit(cuantitativas_precios)
 #write.csv(cuantitativas_precios,"C:/Users/Kevin Macario/Desktop/Uvg/9no Semestre/Mineria de Datos/HT4-Mineria-de-Datos/cuantitativas_precios.csv")
 
 #normalizacion
+normC_precios <- scale(cuantitativas_precios[,2:38], center = TRUE, scale = TRUE)
 
-VALOR <- scale(cuantitativas_precios[,2:38], center = TRUE, scale = TRUE)
+corte <- sample(nrow(normC_precios),nrow(normC_precios)*0.7)
+train<-normC_precios[corte,]
+train<-as.data.frame(train)
 
+test<-normC_precios[-corte,]
+test<-as.data.frame(test)
 
-corte <- sample(nrow(datos),nrow(datos)*0.7)
-train<-datos[corte,]
-test<-datos[-corte,]
+#trabajando con datos Train
+model <- lm(SalePrice ~ LotFrontage + LotArea + GrLivArea + BedroomAbvGr + TotRmsAbvGrd, data = train)
+summary(model)
 
-model <- lm(SalePrice ~ cdata$LotFrontage + cdata$LotArea + cdata$GrLivArea + cdata$BedroomAbvGr + cdata$TotRmsAbvGrd, )
+predModel<-predict(model, newdata = test)
+resultadosTrain<-data.frame(predModel)
 
-#model <- lm(sales ~ prices + facebook + newspaper, data = marketing)
-#summary(model)
+#residuales train
+residuales <- train$SalePrice-predModel
+hist(residuales, main="Distribucion de error de Residuales")
+
+cfm<-confusionMatrix(test,predModel)
+cfm
+
+#sustituyendo variables colienales
+model <- lm(SalePrice ~  LotArea + GrLivArea + TotRmsAbvGrd, data = train)
+summary(model)
+
+predModel<-predict(model, newdata = test)
+resultadosTrain<-data.frame(predModel)
+
+#residuales train
+residuales <- train$SalePrice-predModel
+hist(residuales, main="Distribucion de error de Residuales")
