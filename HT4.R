36f8ac27c0fb1455ee6c904e4b9f2629a6be79a6
Mac17369 <Mac17369@uvg.edu.gt>
2021-03-18 19:01:05 -0600

    Changes

--- HT4.R
+++ HT4.R
@@ -1,9 +1,7 @@
-
-
-setwd("C:/Users/LENOVO/Desktop/Clases/Minería de datos/Github/HT4-Mineria-de-Datos")
+setwd("C:/Users/Kevin Macario/Desktop/Uvg/9no Semestre/Mineria de Datos/HT4-Mineria-de-Datos")
 datatest <- read.csv("house-prices-advanced-regression-techniques/test.csv")
 datatrain <- read.csv("house-prices-advanced-regression-techniques/train.csv")
-
+library(dplyr)
 prices <- read.csv("house-prices-advanced-regression-techniques/sample_submission.csv")
 
 numstest <- sapply(datatest, is.numeric)
@@ -19,4 +17,21 @@ cdata <- bind_rows(cdatatest,cdatatrain)
 cuantitativas_precios<-merge(x = cdata, y = prices, by = "Id")
 cuantitativas_precios <-  na.omit(cuantitativas_precios)
 
-write.csv(cuantitativas_precios,"C:/Users/LENOVO/Desktop/Clases/Minería de datos/Github/HT4-Mineria-de-Datos/cuantitativas_precios.csv")
+write.csv(cuantitativas_precios,"C:/Users/Kevin Macario/Desktop/Uvg/9no Semestre/Mineria de Datos/HT4-Mineria-de-Datos/cuantitativas_precios.csv")
+
+#normalizacion
+
+library(oompaBase)
+library(PreProcess)
+preproc1 <- preProcess(cuantitativas_precios[,], method=c("center", "scale"))
+norm1 <- predict(preproc1, cuantitativas_precios[,])
+summary(norm1)
+
+corte <- sample(nrow(datos),nrow(datos)*0.7)
+train<-datos[corte,]
+test<-datos[-corte,]
+
+model <- lm(SalePrice ~ cdata$LotFrontage + cdata$LotArea + cdata$GrLivArea + cdata$BedroomAbvGr + cdata$TotRmsAbvGrd, )
+
+#model <- lm(sales ~ prices + facebook + newspaper, data = marketing)
+#summary(model)
