b06fa22f1796176fcf552c9fa085e1d03eabbe8a
Pablo Josue Noack Cajbon <noa17596@uvg.edu.gt>
2021-03-18 19:11:12 -0600

    hOLA
    pADRE NUEsTro Ke esTAZ n L cIELO

--- HT4.R
+++ HT4.R
@@ -1,4 +1,5 @@
-setwd("C:/Users/Kevin Macario/Desktop/Uvg/9no Semestre/Mineria de Datos/HT4-Mineria-de-Datos")
+##setwd("C:/Users/Kevin Macario/Desktop/Uvg/9no Semestre/Mineria de Datos/HT4-Mineria-de-Datos")
+setwd("C:/Users/LENOVO/Desktop/Clases/Minería de datos/Github/HT4-Mineria-de-Datos")
 datatest <- read.csv("house-prices-advanced-regression-techniques/test.csv")
 datatrain <- read.csv("house-prices-advanced-regression-techniques/train.csv")
 library(dplyr)
@@ -17,15 +18,12 @@ cdata <- bind_rows(cdatatest,cdatatrain)
 cuantitativas_precios<-merge(x = cdata, y = prices, by = "Id")
 cuantitativas_precios <-  na.omit(cuantitativas_precios)
 
-write.csv(cuantitativas_precios,"C:/Users/Kevin Macario/Desktop/Uvg/9no Semestre/Mineria de Datos/HT4-Mineria-de-Datos/cuantitativas_precios.csv")
+#write.csv(cuantitativas_precios,"C:/Users/Kevin Macario/Desktop/Uvg/9no Semestre/Mineria de Datos/HT4-Mineria-de-Datos/cuantitativas_precios.csv")
 
 #normalizacion
 
-library(oompaBase)
-library(PreProcess)
-preproc1 <- preProcess(cuantitativas_precios[,], method=c("center", "scale"))
-norm1 <- predict(preproc1, cuantitativas_precios[,])
-summary(norm1)
+VALOR <- scale(cuantitativas_precios[,2:38], center = TRUE, scale = TRUE)
+
 
 corte <- sample(nrow(datos),nrow(datos)*0.7)
 train<-datos[corte,]
